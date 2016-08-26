2128a44d3f163f9dbb3d682d4dc3e9dddd4cfeda
vin01188 <vinkang97@gmail.com>
2016-08-26 18:08:13 -0400

    clean more

--- js/index.js
+++ js/index.js
@@ -1,17 +1,9 @@
 $(window).on('resize',function(){
   if($(window).width() >1000){
     $('#navhead').addClass('head');
-    $('#navhead').removeClass('head2')
-    $('#left').addClass('wl');
-    $('#right').addClass('wr');
-        $('#left2').addClass('wl');
-    $('#right2').addClass('wr');
+    $('#navhead').removeClass('head2');
   }else{
     $('#navhead').addClass('head2');
     $('#navhead').removeClass('head');
-    $('#left').removeClass('wl');
-    $('#right').removeClass('wr');
-    $('#left2').removeClass('wl');
-    $('#right2').removeClass('wr');
   }
 })
